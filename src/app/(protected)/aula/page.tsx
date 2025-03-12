c33422b49b30ef7a1566a3069341fb26f893fe5a
Gabriel Diniz <gabriel.diniz@soulcode.com>
2025-03-12 20:08:59 -0300

    SOULDEV-1935 Página Aula

---
+++ src/app/(protected)/aula/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+
+    return (
+        <main>
+            <section className="container container-ajuste mt-5 pt-5">
+            </section>
+        </main>
+    )
+}
