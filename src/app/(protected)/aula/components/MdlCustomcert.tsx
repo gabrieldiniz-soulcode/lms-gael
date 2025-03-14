ca2e36e98399efb2cae68c8f6d212aab2d19177a
Gabriel Diniz <gabriel.diniz@soulcode.com>
2025-03-14 16:35:06 -0300

    SOULDEV-1950 Aula: MdlCustomcert

---
+++ src/app/(protected)/aula/components/MdlCustomcert.tsx
@@ -0,0 +1,27 @@
+import { Button } from "react-bootstrap";
+
+interface Sequence {
+    cmid: number,
+    module: string;
+    complete: boolean;
+    data_module: {
+        name: string;
+        course: number;
+        content: string;
+        externalurl: string;
+        id: number;
+    }
+}
+
+interface Props {
+    sequence: Sequence;
+}
+
+export default function MdlCustomcert({ sequence }: Props) {
+
+    return (
+        <div className="w-100">
+            <Button className="px-3">Baixar Certificado</Button>
+        </div>
+    );
+}
