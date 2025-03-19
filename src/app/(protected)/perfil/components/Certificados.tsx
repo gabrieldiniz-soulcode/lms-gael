6950f5aaed5837a6ef1802b7b003c1f7ef018d2a
Gabriel Diniz <gabriel.diniz@soulcode.com>
2025-03-19 12:49:34 -0300

    SOULDEV-1982 Aula: Certificados

---
+++ src/app/(protected)/perfil/components/Certificados.tsx
@@ -0,0 +1,56 @@
+import { FaChevronRight, FaRegFilePdf } from "react-icons/fa";
+
+export default function Certificados() {
+
+    const certificados = [
+        {
+            titulo: "DESENVOLVEDOR DE JOGOS COM UNITY",
+            link: ""
+        },
+        {
+            titulo: "DESENVOLVEDOR DE JOGOS COM UNITY",
+            link: ""
+        },
+        {
+            titulo: "DESENVOLVEDOR DE JOGOS COM UNITY",
+            link: ""
+        },
+        {
+            titulo: "DESENVOLVEDOR DE JOGOS COM UNITY",
+            link: ""
+        },
+        {
+            titulo: "DESENVOLVEDOR DE JOGOS COM UNITY",
+            link: ""
+        },
+    ]
+
+    return (
+        <div className="row row-gap-4">
+            <div className="d-flex justify-content-between row-gap-4">
+                <span className="fs-28 fw-700 text-auxiliary1-project">Certificados</span>
+                <a href="" className="pt-5 me-3 text-decoration-none d-flex align-items-center gap-2">
+                    Ver todos
+                    <FaChevronRight size={16} />
+                </a>
+            </div>
+            {
+                certificados.map((item, index) => {
+                    if (index > 3) return;
+                    return (
+                        <a href={item.link} target="_blank" className="col-xxl-3 col-md-4 col-md-6 col-10 text-decoration-none" key={index}>
+                            <div className="d-flex flex-column bg-white me-3 rounded-3">
+                                <div className="py-5 px-4 mx-xl-5 mx-md-3 mx-4">
+                                    <FaRegFilePdf className="w-100 h-auto px-4" color="#FF3B30"/>
+                                </div>
+                                <div className="bg-auxiliary5-project pb-3 pt-4 px-4 fw-700">
+                                    {item.titulo}
+                                </div>
+                            </div>
+                        </a>
+                    );
+                })
+            }
+        </div>
+    )
+}
--- src/app/(protected)/perfil/page.tsx
+++ src/app/(protected)/perfil/page.tsx
@@ -4,6 +4,7 @@ import { LoaderContext } from "@/contexts/LoaderContext";
 import { useContext, useEffect } from "react";
 import Perfil from "./components/Perfil";
 import Carreiras from "./components/Carreiras";
+import Certificados from "./components/Certificados";
 
 export default function Page() {
 
@@ -18,8 +19,11 @@ export default function Page() {
             <section className="container container-ajuste mt-5 pt-5">
                 <Perfil />
             </section>
-            <section className="container container-ajuste my-5 pt-2">
+            <section className="container container-ajuste mt-5 pt-2">
                 <Carreiras />
+            </section>
+            <section className="container container-ajuste my-5 pt-2">
+                <Certificados />
             </section>
         </main>
     )
