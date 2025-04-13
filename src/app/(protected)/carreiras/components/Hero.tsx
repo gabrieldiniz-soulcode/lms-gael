487863281b769aa70c9440dcc344f2cb295a2997
Gabriel Diniz <gabriel.diniz@soulcode.com>
2025-04-13 10:39:31 -0300

    SOULDEV-2067 RankingHero

--- src/app/(protected)/carreiras/components/Hero.tsx
+++ src/app/(protected)/carreiras/components/Hero.tsx
@@ -11,6 +11,7 @@ import axios from "axios";
 import bannerDiscord from "/public/discord.png";
 import bannerDiscord2 from "/public/discord_2.png";
 import bannerYT from "/public/ao_vivo_yt.png";
+import trofeu from "/public/trofeu.png";
 
 interface Course {
     id: number;
@@ -35,6 +36,12 @@ export default function Hero() {
     const { user } = useContext(AuthContext);
     const { updateResponses } = useContext(LoaderContext);
     const [course, setCourse] = useState<Course>();
+
+    const [width, setWidth] = useState(90)
+
+    useEffect(() => {
+        setWidth(window?.screen.width > 1580 ? 75 : 65);
+    }, [])
 
     useEffect(() => {
         function getCourse() {
@@ -68,48 +75,65 @@ export default function Hero() {
     return (
         course
         &&
-        <div className="row hero-carreiras row-gap-4">
-            <a href="#" className="col-12 d-md-block d-none px-2">
-                <Image src={bannerDiscord.src} width={0} height={0} className="w-100 h-auto" alt="Banner Discord" />
-            </a>
-            <a href="#" className="col-12 d-md-none d-block px-2">
-                <Image src={bannerDiscord2.src} width={0} height={0} className="w-100 h-auto" alt="Banner Discord" />
-            </a>
-            <div className="col-xxl-6 col-12 card-hero d-md-block d-none">
-                <div className="d-flex box-shadow-hero rounded-3">
-                    <div className="col-5 px-0 rounded-start-3">
-                        <Image src="https://placehold.co/223x288" width={0} height={288} className="w-100 object-fit-cover rounded-start-3" alt="Imagem ilustrativa da carreira" />
-                    </div>
-                    <div className="col-7 bg-white d-flex flex-column py-4 px-3 justify-content-between rounded-end-3">
-                        <span className="fw-700 fs-21 card-title-hero">{course?.fullname}</span>
-                        <span className="card-text-hero">{removeHtmlTags(course?.summary || "")}</span>
-                        <div className="d-flex justify-content-between py-4">
-                            <span className="fs-12 d-flex align-items-center">
-                                <FaRegClock className='text-auxiliary1-project me-2' />
-                                {course?.carga} DE ESTUDO
-                            </span>
-                            <a href={`/curso?id=${course.id}`} className="btn btn-primary d-flex align-items-center justify-content-center fs-12 fw-700 px-md-4">
-                                Acessar Carreira
-                                <RiPlayMiniLine size={20} strokeWidth={0.5} className="ms-1" />
-                            </a>
-                        </div>
-                        <div className="d-flex gap-2 align-items-center fs-12 fw-700">
-                            <div className="w-100">
-                                <ProgressBar now={parseInt(course?.progresso || "0")} />
+        <div className="row hero-carreiras">
+            <div className="col-xxl-11 p-xxl-0 m-xxl-0 pe-xxl-2">
+                <div className="row row-gap-4">
+                    <a href="#" className="col-12 d-md-block d-none px-2">
+                        <Image src={bannerDiscord.src} width={0} height={0} className="w-100 h-auto" alt="Banner Discord" />
+                    </a>
+                    <a href="#" className="col-12 d-md-none d-block px-2">
+                        <Image src={bannerDiscord2.src} width={0} height={0} className="w-100 h-auto" alt="Banner Discord" />
+                    </a>
+                    <div className="col-xxl-6 col-12 card-hero d-md-block d-none">
+                        <div className="d-flex box-shadow-hero rounded-3">
+                            <div className="col-5 px-0 rounded-start-3">
+                                <Image src="https://placehold.co/223x288" width={0} height={288} className="w-100 object-fit-cover rounded-start-3" alt="Imagem ilustrativa da carreira" />
                             </div>
-                            {course.progresso || "0"}%
+                            <div className="col-7 bg-white d-flex flex-column py-4 px-3 justify-content-between rounded-end-3">
+                                <span className="fw-700 fs-21 card-title-hero">{course?.fullname}</span>
+                                <span className="card-text-hero">{removeHtmlTags(course?.summary || "")}</span>
+                                <div className="d-flex justify-content-between py-4">
+                                    <span className="fs-12 d-flex align-items-center">
+                                        <FaRegClock className='text-auxiliary1-project me-2' />
+                                        {course?.carga} DE ESTUDO
+                                    </span>
+                                    <a href={`/curso?id=${course.id}`} className="btn btn-primary d-flex align-items-center justify-content-center fs-12 fw-700 px-md-4">
+                                        Acessar Carreira
+                                        <RiPlayMiniLine size={20} strokeWidth={0.5} className="ms-1" />
+                                    </a>
+                                </div>
+                                <div className="d-flex gap-2 align-items-center fs-12 fw-700">
+                                    <div className="w-100">
+                                        <ProgressBar now={parseInt(course?.progresso || "0")} />
+                                    </div>
+                                    {course.progresso || "0"}%
+                                </div>
+                            </div>
                         </div>
+                    </div>
+                    <div className="col-xxl-6 col-12 card-hero">
+                        <a href="https://www.youtube.com/@SoulCodeAcademy/streams" target="_blank" className="d-flex text-decoration-none flex-column justify-content-center h-100 rounded-3 box-shadow-hero">
+                            <Image src={bannerYT.src} width={0} height={0} className="w-100 h-100 rounded-top-3 overflow-hidden" alt="Imagem ilustrativa da carreira" />
+                            <span className="py-3 bg-white rounded-bottom-3 text-center fs-21 fw-700">
+                                Ao vivo no YouTube
+                                <IoRadioSharp size={24} className="ms-2" />
+                            </span>
+                        </a>
                     </div>
                 </div>
             </div>
-            <div className="col-xxl-6 col-12 card-hero">
-                <a href="https://www.youtube.com/@SoulCodeAcademy/streams" target="_blank" className="d-flex text-decoration-none flex-column justify-content-center h-100 rounded-3 box-shadow-hero">
-                    <Image src={bannerYT.src} width={0} height={0} className="w-100 h-100 rounded-top-3 overflow-hidden" alt="Imagem ilustrativa da carreira" />
-                    <span className="py-3 bg-white rounded-bottom-3 text-center fs-21 fw-700">
-                        Ao vivo no YouTube
-                        <IoRadioSharp size={24} className="ms-2" />
-                    </span>
-                </a>
+            <div className="col-1 d-xxl-block d-none p-0 rounded-3" style={{ overflow: 'hidden' }}>
+                <div className="bg-auxiliary1-project p-2 rounded-3">
+                    <Image src={trofeu.src} width={trofeu.width} height={trofeu.height} alt="troféu" className="w-100 h-auto px-3 py-2" />
+                </div>
+                <div className="bg-auxiliary1-project h-100 p-2 rounded-3 mt-3 d-flex flex-column gap-3 align-items-center">
+                    <h3 className="text-white text-center mt-exxl-3 mt-4 fs-21 fw-700">Rank</h3>
+                    <Image src={"https://placehold.co/65x65"} width={width} height={width} alt="" className="rounded-circle perfil-ranking-aula1" />
+                    <Image src={"https://placehold.co/65x65"} width={width} height={width} alt="" className="rounded-circle perfil-ranking-aula1" />
+                    <Image src={"https://placehold.co/65x65"} width={width} height={width} alt="" className="rounded-circle perfil-ranking-aula1" />
+                    <Image src={"https://placehold.co/65x65"} width={width} height={width} alt="" className="rounded-circle perfil-ranking-aula1" />
+                    <Image src={"https://placehold.co/65x65"} width={width} height={width} alt="" className="rounded-circle perfil-ranking-aula1" />
+                </div>
             </div>
         </div>
     )
--- src/app/(protected)/carreiras/components/Ranking.tsx
+++ src/app/(protected)/carreiras/components/Ranking.tsx
@@ -1,10 +1,17 @@
+import { useEffect, useState } from "react";
+
 import Image from "next/image";
 import img from "/public/rectangle_ranking.png";
 
 export default function Ranking() {
 
-    const largura1 =  window.screen.width > 420 ? 90 : 60; 
-    const largura2 =  window.screen.width > 420 ? 120 : 90; 
+    const [width1, setWidth1] = useState(90)
+    const [width2, setWidth2] = useState(120)
+
+    useEffect(() => {
+        setWidth1(window?.screen.width > 420 ? 90 : 60);
+        setWidth2(window?.screen.width > 420 ? 120 : 90);
+    }, [])
 
     return (
         <div className="bg-auxiliary1-project rounded-3 mx-3" style={{ overflow: 'hidden' }}>
@@ -13,20 +20,20 @@ export default function Ranking() {
                 <div className="position-absolute mt-sm-5 mt-3 d-flex flex-column justify-content-center align-items-center w-100 top-0">
                     <span className="text-white fs-21 fw-700">Ranking</span>
                     <div className="d-flex mt-sm-5 mt-2 gap-3 position-relative">
-                        <div className="mt-5 rounded-circle d-flex flex-column" style={{maxWidth: '100%'}}>
-                            <Image src={"https://placehold.co/90x90"} width={largura1} height={largura1} alt="" className="rounded-circle perfil-ranking-aula2"/>
+                        <div className="mt-5 rounded-circle d-flex flex-column" style={{ maxWidth: '100%' }}>
+                            <Image src={"https://placehold.co/90x90"} width={width1} height={width1} alt="" className="rounded-circle perfil-ranking-aula2" />
                             <div className="d-flex justify-content-center">
                                 <span className="px-2 py-1 rounded-circle colocacao-ranking-aula2 text-white fw-700" style={{ marginTop: -18 }}>2º</span>
                             </div>
                         </div>
                         <div className="mt-2 rounded-circle d-flex flex-column">
-                            <Image src={"https://placehold.co/120x120"} width={largura2} height={largura2} alt="" className="rounded-circle perfil-ranking-aula1" />
+                            <Image src={"https://placehold.co/120x120"} width={width2} height={width2} alt="" className="rounded-circle perfil-ranking-aula1" />
                             <div className="d-flex justify-content-center">
                                 <span className="px-2 py-1 rounded-circle colocacao-ranking-aula1 text-white fw-700" style={{ marginTop: -18 }}>1º</span>
                             </div>
                         </div>
                         <div className="mt-5 rounded-circle d-flex flex-column">
-                            <Image src={"https://placehold.co/90x90"} width={largura1} height={largura1} alt="" className="rounded-circle perfil-ranking-aula3" />
+                            <Image src={"https://placehold.co/90x90"} width={width1} height={width1} alt="" className="rounded-circle perfil-ranking-aula3" />
                             <div className="d-flex justify-content-center">
                                 <span className="px-2 py-1 rounded-circle colocacao-ranking-aula3 text-white fw-700" style={{ marginTop: -18 }}>3º</span>
                             </div>
@@ -36,7 +43,7 @@ export default function Ranking() {
             </div>
             <div className="d-flex flex-column gap-3 mx-4 my-3">
                 <div className="row rounded-3 justify-content-center lista-ranking-aula-active">
-                <div className="py-2 col-2 d-flex align-items-center justify-content-center">
+                    <div className="py-2 col-2 d-flex align-items-center justify-content-center">
                         <span className="p-4 rounded-3 ms-3 colocacao">1°</span>
                     </div>
                     <div className="d-flex col-7 gap-3 align-items-center perfil p-0">
