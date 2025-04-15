f0fa514f0b7a1790a6bece48aae24768f2e5dbfc
Gabriel Diniz <gabriel.diniz@soulcode.com>
2025-04-15 16:47:10 -0300

    SOULDEV-2074 Integração MdlCustomcert

--- src/app/(protected)/aula/components/MdlCustomcert.tsx
+++ src/app/(protected)/aula/components/MdlCustomcert.tsx
@@ -1,4 +1,9 @@
+import { useContext, useState } from "react";
+
+import { AuthContext } from "@/contexts/AuthContext";
 import { Button } from "react-bootstrap";
+import Certificado from "@/components/Certificado/Certificado";
+import axios from "axios";
 
 interface Sequence {
     cmid: number,
@@ -17,11 +22,45 @@ interface Props {
     sequence: Sequence;
 }
 
-export default function MdlCustomcert({ }: Props) {
+interface CertificateData {
+    certificate_created: number;
+    code: string;
+    coursename: string;
+    firstname: string;
+    lastname: string;
+    workload: string;
+}
+
+export default function MdlCustomcert({ sequence }: Props) {
+
+    console.log(sequence)
+
+    const { user } = useContext(AuthContext);
+
+    const [certificado, setCertificado] = useState<CertificateData | null>(null);
+    const [triggerDownload, setTriggerDownload] = useState(false);
+
+    const buscarCertificado = async () => {
+        if (!user?.token) return;
+
+        const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/certificate`, {
+            headers: {
+                Authorization: `Bearer ${user?.token}`,
+                course: sequence.data_module.course
+            }
+        });
+        setCertificado(res.data);
+        setTriggerDownload(true);
+    };
 
     return (
         <div className="w-100">
-            <Button className="px-3">Baixar Certificado</Button>
+            <Button className="px-3" onClick={buscarCertificado}>Baixar Certificado</Button>
+            <Certificado
+                certificado={certificado}
+                triggerDownload={triggerDownload}
+                onDownloaded={() => setTriggerDownload(false)}
+            />  
         </div>
     );
 }
