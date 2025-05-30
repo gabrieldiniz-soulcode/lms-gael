3d673826f015feeca9626cdb1eaf9b61a64e58a7
Cristiano Oliveira <cristiano.oliveira@soulcode.com>
2025-05-29 21:40:10 -0300

    ajuste visual accordion subcurso

--- src/app/(protected)/curso/components/SubCurso.tsx
+++ src/app/(protected)/curso/components/SubCurso.tsx
@@ -51,39 +51,33 @@ export default function SubCurso({ subCurso, carreiraId }: Props) {
         <Accordion defaultActiveKey="0" className="bg-white accordion-curso rounded-3">
             {
                 subCurso
-                &&
-                subCurso.map((sub, index) => (
-                    <Fragment key={index}>
-                        {
-                            sub.name
-                            &&
-                            <Accordion.Item eventKey={index.toString()}>
-                                <Accordion.Header>{sub.name}</Accordion.Header>
-                                <Accordion.Body>
-                                    <div className="d-flex flex-column gap-3">
-                                        {
-                                            sub.sequence.map((aula) => (
-                                                <a href={`/aula?id=${aula.cmid}&cursoId=${sub.course}&carreiraId=${carreiraId}`} key={aula.cmid} className="d-flex align-items-center gap-3 text-decoration-none">
-                                                    {getIcon(aula.module)}
-                                                    {aula?.data_module.name}
-                                                    {
-                                                        aula.complete
-                                                            ?
-                                                            <div className="ms-auto bg-auxiliary9-project rounded-5 d-flex align-items-center justify-content-center">
-                                                                <MdDone size={20} color="#fff" className="m-1" />
-                                                            </div>
-                                                            :
-                                                            <IoIosLock size={24} className="ms-auto" />
-                                                    }
-                                                </a>
-                                            ))
-                                        }
-                                    </div>
-                                </Accordion.Body>
-                            </Accordion.Item>
-                        }
-                    </Fragment>
-                ))
+                    ?.filter((sub) => sub.name)
+                    .map((sub, index) => (
+                        <Accordion.Item eventKey={index.toString()} key={sub.id}>
+                            <Accordion.Header>{sub.name}</Accordion.Header>
+                            <Accordion.Body>
+                                <div className="d-flex flex-column gap-3">
+                                    {sub.sequence.map((aula) => (
+                                        <a
+                                            href={`/aula?id=${aula.cmid}&cursoId=${sub.course}&carreiraId=${carreiraId}`}
+                                            key={aula.cmid}
+                                            className="d-flex align-items-center gap-3 text-decoration-none"
+                                        >
+                                            {getIcon(aula.module)}
+                                            {aula?.data_module.name}
+                                            {aula.complete ? (
+                                                <div className="ms-auto bg-auxiliary9-project rounded-5 d-flex align-items-center justify-content-center">
+                                                    <MdDone size={20} color="#fff" className="m-1" />
+                                                </div>
+                                            ) : (
+                                                <IoIosLock size={24} className="ms-auto" />
+                                            )}
+                                        </a>
+                                    ))}
+                                </div>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    ))
             }
         </Accordion>
     )
